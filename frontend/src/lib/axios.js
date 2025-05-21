e1abbea759682bffd99410b0bd381cef4b862b50
Vipash0006 <vipasharora06@gmail.com>
2025-05-21 16:19:48 +0530

    added changes in frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || (import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://realchat-wbi3.onrender.com");
+const BASE_URL = import.meta.env.VITE_API_URL || (import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://talkly.onrender.com");
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
